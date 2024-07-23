364ef846e5d864c46106f6bb9e02e2c2378cda22
Amirsalar <amirsalar.alipoor@gmail.com>
2024-07-23 17:32:48 +0330

    Add project files.

---
+++ URLEncoder/UrlEncodeFunction.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlTypes;
+
+
+
+public static class UrlEncodeFunction
+{
+    public static SqlString UrlEncode(SqlString input)
+    {
+        string encodedString = System.Uri.EscapeDataString(input.Value);
+        return new SqlString(encodedString);
+    }
+}
+
